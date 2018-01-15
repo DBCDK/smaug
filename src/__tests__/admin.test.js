1acd64a084cf6039536a55af4a772e9f40a192cb
Soeren Vibjerg <vibjerg@gmail.com>
2018-01-15 10:42:14 +0100

    add tests of admin token endpoint

--- src/__tests__/admin.test.js
+++ src/__tests__/admin.test.js
@@ -5,7 +5,13 @@ import chaiAsPromised from 'chai-as-promised';
 import Chance from 'chance';
 import request from 'supertest';
 import {createAdminApp, filterClient, clientWithId} from '../expressapp';
+import AllowAllUserStore from '../oauth/userstore/allow-all';
+import DenyAllUserStore from '../oauth/userstore/deny-all';
+import TokenStore from '../oauth/tokenstore/inmemory';
 import ClientStore from '../oauth/clientstore/inmemory';
+import UserStore from '../oauth/userstore/inmemory';
+import {userEncode} from '../utils';
+
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -20,17 +26,24 @@ describe('admin app', function () {
   var app = null;
   var chance = null;
   var appConfig = null;
-  var username = null;
-  var password = null;
+  var admin_username = null;
+  var admin_password = null;
   var clientId = null;
   var clientSecret = null;
   var client = null;
+  var user;
+  var username;
+  var password;
+
 
   before(function () {
     chance = new Chance();
-    username = chance.word({length: 10});
-    password = chance.string();
+    admin_username = chance.word({length: 10});
+    admin_password = chance.string();
     client = {name: 'a-client', config: {}, contact: {owner: {name: '', phone: '', email: ''}}};
+    user = {id: chance.word({length: 10}), libraryId: '123456'};
+    username = userEncode(user.libraryId, user.id);
+    password = chance.string();
 
     appConfig = {
       admin: {
@@ -38,8 +51,7 @@ describe('admin app', function () {
       }
     };
 
-    appConfig.admin.users[username] = password;
-
+    appConfig.admin.users[admin_username] = admin_password;
     var stores = {};
     stores.clientStore = new ClientStore(stores, {
       clients: {
@@ -49,9 +61,12 @@ describe('admin app', function () {
         }
       }
     });
-
+    stores.tokenStore = new TokenStore(stores);
+    stores.userStore = new UserStore(stores);
+    stores.userStore.storeUser(username, password);
     app = createAdminApp(appConfig);
     app.set('stores', stores);
+    app.set('auth', {default: stores.userStore, allowAll: new AllowAllUserStore(), denyAll: new DenyAllUserStore()});
   });
 
   describe('auth', function() {
@@ -70,21 +85,21 @@ describe('admin app', function () {
     it('should respond with 401 with wrong username', function (done) {
       request(app)
         .get('/clients')
-        .auth(username + 'foo', password)
+        .auth(admin_username + 'foo', admin_password)
         .expect(401, done);
     });
 
     it('should respond with 401 with wrong password', function (done) {
       request(app)
         .get('/clients')
-        .auth(username, password + 'foo')
+        .auth(admin_username, admin_password + 'foo')
         .expect(401, done);
     });
 
     it('should respond with 200 with correct credentials', function (done) {
       request(app)
         .get('/clients')
-        .auth(username, password)
+        .auth(admin_username, admin_password)
         .expect(200, done);
     });
   });
@@ -93,7 +108,7 @@ describe('admin app', function () {
     it('should create a client', function(done) {
       request(app)
         .post('/clients')
-        .auth(username, password)
+        .auth(admin_username, admin_password)
         .type('json')
         .send(JSON.stringify(client))
         .expect((res) => {
@@ -108,7 +123,7 @@ describe('admin app', function () {
       client.name = 'an updated client';
       request(app)
         .put('/clients/' + clientId)
-        .auth(username, password)
+        .auth(admin_username, admin_password)
         .type('json')
         .send(JSON.stringify(client))
         .expect((res) => {
@@ -120,7 +135,7 @@ describe('admin app', function () {
     it('should retrieve a client', function(done) {
       request(app)
         .get('/clients/' + clientId)
-        .auth(username, password)
+        .auth(admin_username, admin_password)
         .expect((res) => {
           res.body.should.have.property('id', clientId);
           res.body.should.have.property('name', client.name);
@@ -129,17 +144,53 @@ describe('admin app', function () {
         .expect(200, done);
     });
 
+    it('should retrieve a client token', function(done) {
+      request(app)
+        .post('/clients/token/' + clientId)
+        .auth(admin_username, admin_password)
+        .type('json')
+        .send(JSON.stringify({
+          grant_type: 'password',
+          username: '@',
+          password: '@'
+        }))
+        .expect((res) => {
+          res.body.should.have.property('token_type');
+          res.body.should.have.property('access_token');
+          res.body.should.have.property('expires_in');
+        })
+        .expect(200, done);
+    });
+
+    it('should retrieve an authenticated client token', function(done) {
+      request(app)
+        .post('/clients/token/' + clientId)
+        .auth(admin_username, admin_password)
+        .type('json')
+        .send(JSON.stringify({
+          grant_type: 'password',
+          username: username,
+          password: password
+        }))
+        .expect((res) => {
+          res.body.should.have.property('token_type');
+          res.body.should.have.property('access_token');
+          res.body.should.have.property('expires_in');
+        })
+        .expect(200, done);
+    });
+
     it('should delete a client', function(done) {
       request(app)
         .delete('/clients/' + clientId)
-        .auth(username, password)
+        .auth(admin_username, admin_password)
         .expect(200, done);
     });
 
     it('should list all clients', function(done) {
       request(app)
         .get('/clients')
-        .auth(username, password)
+        .auth(admin_username, admin_password)
         .expect((res) => {
           res.body.should.have.lengthOf(1);
           res.body.forEach((returnedClient) => {
