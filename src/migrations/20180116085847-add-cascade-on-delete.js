9ba401571214a1c8f3714d23fa6cb2fa79be6cad
Soeren Vibjerg <vibjerg@gmail.com>
2018-01-30 08:27:17 +0100

    Only drop constraint if it exists

--- src/migrations/20180116085847-add-cascade-on-delete.js
+++ src/migrations/20180116085847-add-cascade-on-delete.js
@@ -3,13 +3,13 @@
 module.exports = {
   up: function (queryInterface) {
     return queryInterface.sequelize.query(
-      'ALTER TABLE tokens DROP CONSTRAINT "tokens_clientId_fkey", ADD CONSTRAINT "tokens_clientId_fkey" foreign key ("clientId") references clients (id) ON DELETE CASCADE'
+      'ALTER TABLE tokens DROP CONSTRAINT IF EXISTS "tokens_clientId_fkey", ADD CONSTRAINT "tokens_clientId_fkey" foreign key ("clientId") references clients (id) ON DELETE CASCADE'
     );
   },
 
   down: function (queryInterface) {
     return queryInterface.sequelize.query(
-      'ALTER TABLE tokens DROP CONSTRAINT "tokens_clientId_fkey", ADD CONSTRAINT "tokens_clientId_fkey" foreign key ("clientId") references clients (id)'
+      'ALTER TABLE tokens DROP CONSTRAINT IF EXISTS "tokens_clientId_fkey", ADD CONSTRAINT "tokens_clientId_fkey" foreign key ("clientId") references clients (id)'
     );
   }
 };
