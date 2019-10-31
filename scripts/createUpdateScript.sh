6a82237b39caf213fe1afef0c535ea4b0538b228
Nikolaj Winther <niw@dbc.dk>
2019-10-31 14:16:14 +0100

    updated scripts to handle several urls

--- scripts/createUpdateScript.sh
+++ scripts/createUpdateScript.sh
@@ -6,14 +6,15 @@
 if [ $# -lt 3 ]
 then
     echo 'Please call this script with 3 parameters. Examples:'
-    echo './createUpdateScript.sh "user:password" "https://auth-admin-stg.dbc.dk" ~/indata/idsFile'
-    echo './createUpdateScript.sh "user:password" "https://auth-admin.dbc.dk" ~/indata/idsFile'
+    echo './createUpdateScript.sh "user:password" "https://auth-admin-stg.dbc.dk" ~/indata/smaug-id-libraries ~/indata/smaug-information.tsv'
+    echo './createUpdateScript.sh "user:password" "https://auth-admin.dbc.dk" ~/indata/smaug-id-libraries ~/indata/smaug-information.tsv'
     exit 1
 fi
 
 smaugUserPwd=$1
 smaugAdminUrl=$2
 idsFile=$3
+bibFile=$4
 
 echo "#!/usr/bin/env bash"
 echo ""
@@ -23,6 +24,9 @@ for id in $ids
 do
     #echo "echo update client data for id: $id"
     smaugObject=$(curl -X GET --user $smaugUserPwd $smaugAdminUrl/clients/${id} 2>/dev/null)
-    #echo smaugObject: $smaugObject #test that an object is collected
-    ./updateSmaugClients.js -o "$smaugObject" -p $smaugUserPwd -s $smaugAdminUrl
+#    echo smaugObject: $smaugObject #test that an object is collected
+#    displayName=$(echo $smaugObject | json_pp | grep displayName | cut -d '"' -f4)
+    bibArr=$(cat $bibFile | grep "$id" | cut -d "," -f2,7-10)
+#    echo "$bibArr"
+    ./updateSmaugClients.js -o "$smaugObject" -p $smaugUserPwd -s $smaugAdminUrl -b "$bibArr"
 done
--- scripts/updateSmaugClients.js
+++ scripts/updateSmaugClients.js
@@ -6,7 +6,7 @@
  * Then creates a curl string that can be used to update that id in smaug.
  * Created together with createUpdateScript.sh that calls this script
  * Should you run it from a bash it is done like this:
- * ./updateSmaugClients.js -s "https://auth-admin-stg.dbc.dk" -p "admin:password" -o '{ "id": "c9520dff-1685-46ea-bfe9-d2a24651bc6f", "config": { } }'
+ * ./updateSmaugClients.js -s "https://auth-admin-stg.dbc.dk" -p "admin:password" -o '{ "id": "c9520dff-1685-46ea-bfe9-d2a24651bc6f", "config": { } } -b "721700,Helsingør Bibliotek,https://helsbib.dk"'
  *
  */
 
@@ -15,20 +15,74 @@ const option = getOptions();
 
 const smaugObject = JSON.parse(option.smaugObject);
 const id = smaugObject.id;
+var bibArray = option.bibArray.split(",");
+const agencyId = bibArray.shift();
 
-// If config is supposed to be forcibly updated, then use the block
-var updateObjectKey = "grants";
-var updateObjectContent = ["authorization_code", "password"];
-smaugObject["config"][updateObjectKey] = updateObjectContent;
 
-// If config is only to be updated when empty, then use the block
-updateObjectKey = "redirectUris";
-updateObjectContent = ["https://*/adgangsplatformen/callback", "https://*/", "https://*"];
-if (!smaugObject["config"][updateObjectKey]) {
+// indsæt agencyId
+var updateObjectKey = "agencyId";
+var updateObjectContent = agencyId;
+// skal der oprettes agencyId med tom streng hvis biblioteket ikke har et agencyId?
+// smaugObject["config"][updateObjectKey] = updateObjectContent;
+
+// skal der kun oprettes agencyId hvis biblioteket har et agencyId?
+if (updateObjectContent) {
   smaugObject["config"][updateObjectKey] = updateObjectContent;
-} else {
-  console.log("echo " + updateObjectKey + " is already set for id: " + id + ". Contains: " + smaugObject.config[updateObjectKey]);
 }
+
+// opdater redirectUris
+updateObjectKey = "redirectUris";
+var hasReturnUrl = false;
+smaugObject["config"][updateObjectKey] = [];
+const https = "https://";
+const callback = "/adgangsplatformen/callback";
+const slash = "/";
+for (var i = 0; i < bibArray.length; i++) {
+  updateObjectContent = bibArray[ i ];
+  if ( updateObjectContent ) {
+    hasReturnUrl = true;
+    smaugObject["config"][updateObjectKey].push(https + updateObjectContent + callback);
+    smaugObject["config"][updateObjectKey].push(https + updateObjectContent + slash);
+    smaugObject["config"][updateObjectKey].push(https + updateObjectContent);
+  }
+}
+if (!hasReturnUrl) {
+  smaugObject["config"][updateObjectKey] = ["https://*/adgangsplatformen/callback", "https://*/", "https://*"];
+}
+
+//indsæt attributes object i config.
+smaugObject["config"]["attributes"] = {
+  "cpr": {
+    "name": "CPR-nummer",
+      "description": "Brugerens CPR-nummer"
+  },
+  "userId": {
+    "name": "Biblioteks bruger-id",
+      "description": "Brugerens identitet på biblioteket - oftest CPR-nummer"
+  },
+  "pincode": {},
+  "uniqueId": {
+    "name": "bruger ID",
+      "description": "Unikt bruger ID, som ikke er personhenførbar"
+  },
+  "libraries": {
+    "name": "Biblioteker",
+      "description": "En liste over de biblioteker som kender brugeren"
+  },
+  "municipality": {
+    "name": "Kommunenummer",
+      "description": "3 cifret kommunenummer"
+  }
+};
+
+// If config is only to be updated when empty, then use this block of code
+// updateObjectKey = "redirectUris";
+// updateObjectContent = returnUrl;
+// if (!smaugObject["config"][updateObjectKey]) {
+//   smaugObject["config"][updateObjectKey] = updateObjectContent;
+// } else {
+//   console.log("echo " + updateObjectKey + " is already set for id: " + id + ". Contains: " + smaugObject.config[updateObjectKey]);
+// }
 
 
 const curlLine = "curl -X PUT -H \"Content-Type: application/json\" --user " + option.smaugUserPwd + " " +  option.smaugAdminUrl + "/clients/" + id + " -d '{\"config\":" + JSON.stringify(smaugObject.config) + "}'";
@@ -43,6 +97,7 @@ console.log(curlLine);
 function getOptions() {
   const ops = stdio.getopt({
     smaugObject:{key: 'o', args: 1, description: 'smaug object'},
+    bibArray:{key: 'b', args: 1, description: 'library information array'},
     smaugUserPwd: {key: 'p', args: 1, description: 'smaug admin user:password'},
     smaugAdminUrl: {key: 's', args: 1, description: 'smaug admin endpoint, ex: https://auth-admin-stg.dbc.dk'}
   });
